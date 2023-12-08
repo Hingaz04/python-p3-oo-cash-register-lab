f98bf572ab6fbdc8ec807f97ce800dd08dd97fc7
Hingaz <hhinga04@gmail.com>
2023-12-08 16:10:19 +0300

    Completed Lab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.total = 0
+        self.discount = discount
+        self.items = []
+        self.last_transaction_amount = 0
+
+    def add_item(self, item, price, quantity=1):
+
+        pass
+
+    def apply_discount(self):
+
+        pass
+
+    def void_last_transaction(self):
+
+        pass
